d7bb972314dd2644b74bc4dfa53aedc61c01aac7
jaehyeon48 <imjaehyeon48@gmail.com>
2020-08-10 09:03:16 +0900

    connected to store from Navbar and added main logo image

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link, withRouter } from 'react-router-dom';
 
 import './navbar.css';
+import logo from '../../images/tyros_logo_white.png';
 
-export default function Navbar() {
+const Navbar = ({
+  isAuthenticated
+}) => {
   return (
     <nav className="navbar navbar--dark-theme">
-      Initial navbar component
+      <img src={logo} alt="main logo" />
     </nav>
   );
 }
+
+Navbar.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps)(withRouter(Navbar));

be8dbb3b2cee1a8fbf7d9a55ac050fff919a84d6
jaehyeon48 <imjaehyeon48@gmail.com>
2020-08-10 19:32:25 +0900

    Completed Navbar guest render

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
@@ -13,10 +13,18 @@ const Navbar = ({
   user,
   history
 }) => {
+  const sideBarRef = useRef();
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-  const handleClickBars = () => {
-    setIsSidebarOpen(!isSidebarOpen);
+  const handleClickSidebar = async () => {
+    if (isSidebarOpen) {
+      sideBarRef.current.style = 'animation: closeSidebar 500ms';
+      await new Promise(resolve => setTimeout(resolve, 400)); // wait for animation
+      setIsSidebarOpen(!isSidebarOpen);
+    }
+    else {
+      setIsSidebarOpen(!isSidebarOpen);
+    }
   };
 
   const handleClickLogo = () => {
@@ -27,11 +35,18 @@ const Navbar = ({
     <React.Fragment>
       <Link to="/signup" className="navbar-signup">Sign Up</Link>
       <Link to="login" className="navbar-login">Login</Link>
-      <div className="navbar-bars" onClick={handleClickBars}>
+      <div className="navbar-bars-icon" onClick={handleClickSidebar}>
         <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" class="svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>
       </div>
       {isSidebarOpen ? (
-        <div className="navbar-sidebar"></div>
+        <div ref={sideBarRef} className={`navbar-sidebar ${theme === 'dark' ? 'sidebar--dark-theme' : 'sidebar--light-theme'}`}>
+          <div className="sidebar-content sidebar-signup">
+            <Link to="signup" onClick={handleClickSidebar}>Sign Up</Link>
+          </div>
+          <div className="sidebar-content sidebar-login">
+            <Link to="login" onClick={handleClickSidebar}>Login</Link>
+          </div>
+        </div>
       ) : null}
     </React.Fragment>
   );

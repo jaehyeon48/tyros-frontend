2c4f4a82f48e77b7810aac251cc11c8c6712412c
jaehyeon48 <imjaehyeon48@gmail.com>
2020-08-11 18:52:03 +0900

    Connected MainPage to store

--- src/components/mainpage/MainPage.js
+++ src/components/mainpage/MainPage.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 import './mainpage.css';
 
-export default function MainPage() {
+const MainPage = ({
+  isAuthenticated
+}) => {
+  if (!isAuthenticated) {
+    return <Redirect to="/login" />
+  }
+
   return (
     <div className="main-container">
       Initial Main Page
     </div>
   );
 }
+
+MainPage.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps)(MainPage);

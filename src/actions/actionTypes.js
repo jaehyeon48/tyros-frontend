44bfc09dc7b6ff666f851f749ba1720fed287992
jaehyeon48 <imjaehyeon48@gmail.com>
2020-08-12 21:48:55 +0900

    Added portfolio reducer

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -6,3 +6,10 @@ export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const USER_LOADED = 'USER_LOADED';
 export const AUTH_FAIL = 'AUTH_FAIL';
 export const LOGOUT = 'LOGOUT';
+
+/* Portfolio */
+export const SELECT_PORTFOLIO = 'SELECT_PORTFOLIO';
+export const LOAD_PORTFOLIO = 'LOAD_PORTFOLIO';
+export const CREATE_PORTFOLIO = 'CREATE_PORTFOLIO';
+export const EDIT_PORTFOLIO = 'EDIT_PORTFOLIO';
+export const DELETE_PORTFOLIO = 'DELETE_PORTFOLIO';
--- src/reducers/indexReducer.js
+++ src/reducers/indexReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import auth from './authReducer';
+import portfolio from './portfolioReducer';
 
 export default combineReducers({
-  auth
+  auth,
+  portfolio
 });

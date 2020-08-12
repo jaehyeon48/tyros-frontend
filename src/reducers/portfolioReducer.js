edd1e880b7cbba29944298dd9b4353ecc57e3e83
jaehyeon48 <imjaehyeon48@gmail.com>
2020-08-12 22:13:42 +0900

    Added portfolio error action types

--- src/reducers/portfolioReducer.js
+++ src/reducers/portfolioReducer.js
@@ -3,7 +3,10 @@ import {
   LOAD_PORTFOLIO,
   CREATE_PORTFOLIO,
   EDIT_PORTFOLIO,
-  DELETE_PORTFOLIO
+  DELETE_PORTFOLIO,
+  PORTFOLIO_LOAD_ERROR,
+  PORTFOLIO_EDIT_ERROR,
+  PORTFOLIO_DELETE_ERROR
 } from '../actions/actionTypes'
 
 const initialState = {
@@ -19,12 +22,20 @@ export default function portfolioReducer(state = initialState, action) {
       return {
         ...state,
         currentPortfolio: payload
-      }
+      };
     case LOAD_PORTFOLIO:
       return {
         ...state,
         portfolioList: payload
       };
+    case PORTFOLIO_LOAD_ERROR:
+      return {
+        ...state,
+        portfolioList: [],
+        currentPortfolio: null
+      };
+    case PORTFOLIO_EDIT_ERROR:
+    case PORTFOLIO_DELETE_ERROR:
     case CREATE_PORTFOLIO:
     case EDIT_PORTFOLIO:
     case DELETE_PORTFOLIO:

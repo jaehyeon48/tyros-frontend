353213516a47795369825cec2553851fb15eae99
jaehyeon48 <imjaehyeon48@gmail.com>
2020-08-12 14:12:38 +0900

    SERVER_URL local version

--- src/actions/serverURL.js
+++ src/actions/serverURL.js
@@ -1,3 +1,3 @@
-const SERVER_URL = 'https://tyros.herokuapp.com'; // for production
-// const SERVER_URL = 'http://localhost:5000'; // for development
+// const SERVER_URL = 'https://tyros.herokuapp.com'; // for production
+const SERVER_URL = 'http://localhost:5000'; // for development
 export default SERVER_URL;

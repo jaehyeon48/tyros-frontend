44972fb60dc5a350b5c349c1349413cffe466d5e
jaehyeon48 <imjaehyeon48@gmail.com>
2020-08-13 18:34:32 +0900

    Added add new portfolio input form skeleton

--- src/components/user/ManagePortfolio.js
+++ src/components/user/ManagePortfolio.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
@@ -18,16 +18,38 @@ const ManagePortfolio = ({
   useEffect(() => { loadPortfolios() }, [loadPortfolios]);
   useEffect(() => { selectCurrentPortfolio() }, [selectCurrentPortfolio]);
 
+  const [newPortfolioName, setNewPortfolioName] = useState('');
+
+  const handleChange = (e) => {
+    setNewPortfolioName(e.target.value);
+  }
+
+  const handleAddNewPortfolio = () => {
+
+  }
+
   return (
-    <div className="portfolios-container">
-      {portfolioList.map((portfolio) => (
-        <PortfolioItem
-          key={portfolio.portfolioId}
-          portfolio={portfolio}
-          currentPortfolio={currentPortfolio}
+    <React.Fragment>
+      <div className="new-portfolio-form-container">
+        <label className="new-portfolio-label" onClick={handleAddNewPortfolio}>Add New Portfolio: </label>
+        <input
+          type="text"
+          value={newPortfolioName}
+          onChange={handleChange}
+          className="new-portfolio-form-field"
         />
-      ))}
-    </div>
+        <button className="btn btn-new-portfolio">ADD NEW PORTFOLIO</button>
+      </div>
+      <div className="portfolios-container">
+        {portfolioList.map((portfolio) => (
+          <PortfolioItem
+            key={portfolio.portfolioId}
+            portfolio={portfolio}
+            currentPortfolio={currentPortfolio}
+          />
+        ))}
+      </div>
+    </React.Fragment>
   );
 }
 

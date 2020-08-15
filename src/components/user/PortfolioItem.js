9399dd0abcd4593dcd116d15dac2df72e0775191
jaehyeon48 <imjaehyeon48@gmail.com>
2020-08-15 18:35:59 +0900

    Added portfolio edit modal

--- src/components/user/PortfolioItem.js
+++ src/components/user/PortfolioItem.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import Modal from '../modal/Modal';
 import { deletePortfolio } from '../../actions/portfolioAction';
 
 const PortfolioItem = ({
@@ -9,20 +10,51 @@ const PortfolioItem = ({
   currentPortfolio,
   deletePortfolio
 }) => {
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [editPortfolioName, setEditPortfolioName] = useState(portfolio.portfolioName);
+
+  const openEditModal = () => {
+    setIsEditModalOpen(true);
+  }
+
+  const closeEditModal = () => {
+    setIsEditModalOpen(false);
+  }
+
+  const handleEditPortfolio = () => {
+
+  }
+
   const handleDeletePortfolio = () => {
     deletePortfolio(portfolio.portfolioId);
   }
 
   return (
-    <div className="portfolio-item">
-      <div className="portfolio-name">{portfolio.portfolioName}</div>
-      <button className="btn btn-portfolio-edit">EDIT</button>
-      <button className="btn btn-portfolio-delete" onClick={handleDeletePortfolio}>DELETE</button>
-      <span className={
-        `select-notice 
+    <React.Fragment>
+      <div className="portfolio-item">
+        <div className="portfolio-name">{portfolio.portfolioName}</div>
+        <button className="btn btn-portfolio-edit" onClick={openEditModal}>EDIT</button>
+        <button className="btn btn-portfolio-delete" onClick={handleDeletePortfolio}>DELETE</button>
+        <span className={
+          `select-notice 
         ${portfolio.portfolioId === currentPortfolio
-          ? "selected-portfolio" : "not-selected-portfolio"}`}>SELECTED</span>
-    </div>
+            ? "selected-portfolio" : "not-selected-portfolio"}`}>SELECTED</span>
+      </div>
+      {isEditModalOpen ? (
+        <Modal closeModalFunc={closeEditModal}>
+          <div className="portfolio-form">
+            <label className="portfolio-form-label">Portfolio Name: </label>
+            <input
+              type="text"
+              value={editPortfolioName}
+              onChange={setEditPortfolioName}
+              className="portfolio-form-field"
+            />
+            <button className="btn portfolio-form-edit-btn">EDIT</button>
+          </div>
+        </Modal>
+      ) : null}
+    </React.Fragment>
   )
 }
 

024c8beaa1eb08f50cf7b35e881d6003044260a4
jaehyeon48 <imjaehyeon48@gmail.com>
2020-08-20 14:22:47 +0900

    Removed redundant async keyword and added settimeout to close alert after 3s

--- src/actions/alertAction.js
+++ src/actions/alertAction.js
@@ -3,13 +3,16 @@ import {
   HIDE_ALERT
 } from './actionTypes';
 
-export const showAlert = (alertMessage, alertType) => async (dispatch) => {
+export const showAlert = (alertMessage, alertType) => (dispatch) => {
   dispatch({
     type: SHOW_ALERT,
     payload: { alertMessage, alertType }
   });
+  setTimeout(() => {
+    dispatch(hideAlert());
+  }, 3000);
 }
 
-export const hideAlert = () => async (dispatch) => {
+export const hideAlert = () => (dispatch) => {
   dispatch({ type: HIDE_ALERT });
 }

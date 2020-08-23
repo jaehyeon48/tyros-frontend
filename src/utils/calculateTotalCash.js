060e2c144975f51f8fe8cd6cf20a758113b6ffbf
jaehyeon48 <imjaehyeon48@gmail.com>
2020-08-23 13:18:43 +0900

    modified function's logic to return 0 when total is below 0

--- src/utils/calculateTotalCash.js
+++ src/utils/calculateTotalCash.js
@@ -2,10 +2,7 @@ export const calculateTotalCashAmount = (cashList) => {
   let totalCashAmount = 0;
 
   cashList.map(cash => {
-    if (totalCashAmount < 0) {
-      return 0;
-    }
-    else if (cash.transactionType === 'deposit') {
+    if (cash.transactionType === 'deposit') {
       totalCashAmount += cash.amount;
     }
     else if (cash.transactionType === 'withdraw') {
@@ -13,5 +10,6 @@ export const calculateTotalCashAmount = (cashList) => {
     }
   });
 
+  if (totalCashAmount < 0) return 0;
   return Number(totalCashAmount.toFixed(2));
 }

a7d71da1850d3e61c25464116c985acb8f9d22f2
jaehyeon48 <imjaehyeon48@gmail.com>
2020-08-26 15:50:37 +0900

    added edit specific position record

--- src/components/position/PositionDetail.js
+++ src/components/position/PositionDetail.js
@@ -1,16 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import StockGroupItem from './StockGroupItem';
+import Modal from '../modal/Modal';
 import { getStocksByTickerGroup } from '../../actions/stockAction';
 import './positionDetail.css';
+import EditPosition from './EditPosition';
 
 const PositionDetail = ({
   match,
+  stockGroup,
   getStocksByTickerGroup
 }) => {
   const PORTFOLIO_ID = match.params.portfolioId;
   const TICKER = match.params.ticker;
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [formData, setFormData] = useState({
+    stockId: '',
+    ticker: TICKER.toUpperCase(),
+    price: '',
+    quantity: '',
+    transactionType: '',
+    transactionDate: ''
+  });
 
   useEffect(() => {
     if (PORTFOLIO_ID && TICKER) {
@@ -18,9 +32,45 @@ const PositionDetail = ({
     }
   }, [PORTFOLIO_ID, TICKER]);
 
+  const openEditModal = () => {
+    setIsModalOpen(true);
+  }
+
+  const closeEditModal = () => {
+    setIsModalOpen(false);
+  }
+
   return (
-    <div>
-    </div>
+    <React.Fragment>
+      <div className="stock-group-container">
+        <div className="stock-group-header">
+          <span className="stock-group-header-price">Price</span>
+          <span className="stock-group-header-quantity">Quantity</span>
+          <span className="stock-group-header-type">Type</span>
+          <span className="stock-group-header-date">Date</span>
+        </div>
+        {stockGroup && stockGroup.map(item => (
+          <StockGroupItem
+            key={item.stockId}
+            stockId={item.stockId}
+            price={item.price}
+            quantity={item.quantity}
+            transactionType={item.transactionType}
+            transactionDate={new Date(item.transactionDate).toJSON().slice(0, 10)}
+            formData={formData}
+            openEditModal={openEditModal}
+            setFormData={setFormData}
+          />))}
+      </div>
+      {isModalOpen && (
+        <Modal closeModalFunc={closeEditModal}>
+          <EditPosition
+            formData={formData}
+            setFormData={setFormData}
+          />
+        </Modal>
+      )}
+    </React.Fragment>
   );
 }
 
@@ -29,6 +79,8 @@ PositionDetail.propTypes = {
   getStocksByTickerGroup: PropTypes.func,
 };
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+  stockGroup: state.stock.stockGroup
+});
 
 export default connect(mapStateToProps, { getStocksByTickerGroup })(PositionDetail);
